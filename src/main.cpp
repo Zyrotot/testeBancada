8bd1c4ca4d14cca38174f775c5448ce1a788ec97
Mathias <mathias.nichele@mytapp.com.br>
2023-04-20 14:02:53 -0300

    Retirando coisas do LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,4 @@
 #include <Arduino.h>
-#include <LiquidCrystal.h> // Biblioteca para utilizar o display lcd
-
-// Ligações no Arduino do display
-// const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-// LiquidCrystal lcd (rs, en, d4, d5, d6, d7);
 
 #define dirPin 2 // classifica pino que define a direção do motor
 #define stepPin 3
@@ -11,12 +6,6 @@
 #define stepDelay 100
 
 void setup() {
-
-  // lcd.begin(16, 2); // Aqui informa que são 16 caracteres e 2 linhas
-  // lcd.setCursor(0, 0); // Colando pra exibir a frase a partir da coluna 0 e linha 0 (opcional)
-  // lcd.print("Bancada de Teste");
-  // lcd.serCursor(0, 1); // Colando pra exibir a frase a partir da coluna 0 e linha 1
-  // lcd.print("Due Laser");
 
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
